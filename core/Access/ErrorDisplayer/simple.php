55dec3fbe5d494b56f3a19744250e49030c1d909
Youssefhad <youssefhad2@gmail.com>
2015-01-11 23:31:40 +0000

    not important and add new simple error displayer

---
+++ core/Access/ErrorDisplayer/simple.php
@@ -0,0 +1,30 @@
+<head>
+	<meta charset="utf-8"/>
+	<title>Ohlala! Il y avait une erreur</title>
+	<style type="text/css">
+	body
+	{
+		background: #e9e9e9;
+		margin: 0px;
+		padding: 0px;
+	}
+
+	div	
+	{
+		border:1px solid gray;
+		border-radius:5px;
+		display: inline-block;
+		padding:30px;
+		font-size: 16px;
+		font: 20px Georgia, "Times New Roman", Times, serif;
+		width: 460px;
+		margin: 60px auto;
+		display: block;
+		background: white;
+	}
+	</style>
+
+</head>
+<body>
+	<div><?php echo $msg ?></div>
+</body>
