e56d93d189e6dc331f73a652440133fee22c6983
youssefhad <youssefhad2@gmail.com>
2015-01-16 11:41:11 +0000

    implementation of Mail class to be adapted with SwiftMailer

--- core/Mail_2.php
+++ core/Mail_2.php
@@ -7,13 +7,13 @@ class Mail
 {
 	private $transport=null;
 	private $too=null;
-	private $atachmnt=null;
+	private $attachmnt=null;
 	private $subject=null;
 	private $froms=null;
 	private $replay=null;
 	private $cc=null;
 	private $cci=null;
-	private $type="html";
+	//private $type="html";
 	private static $error="";
 	//
 	// SMTP Config
@@ -23,25 +23,28 @@ class Mail
 	private $type="html";
 	private $username="";
 	private $password="";
+	private $from=array();
 
 
 	static function configured($self)
 	{
-		if(!empty($self->host) || !empty($self->port) || !empty($self->secure) || !empty($self->username) || !empty($self->password))
+		if(!empty($self->host) || !empty($self->port) || !empty($self->secure) || !empty($self->username) || !empty($self->password) || !empty($self->from))
 		{
-			// if($self->type!="html" && $self->type!="text") 
-			// { 
-			// 	throw new InvalidArgumentException("The Mail Type should Html or Text a Bourass", 1); 
-			// 	return false;
-			// }
-			// else
-			// {
-			// 	return true;
-			// }
 			return true;
 			
 		}
 		else return false;
+	}
+
+	static function check($selfmail)
+	{
+		if(empty($selfmail->host)) throw new InvalidArgumentException("Missing smtp host");
+		if(empty($selfmail->port)) throw new InvalidArgumentException("Missing smtp port"); 
+		if(empty($selfmail->secure)) throw new InvalidArgumentException("Missing smtp secure");
+		if(empty($selfmail->username)) throw new InvalidArgumentException("Missing smtp username");
+		if(empty($selfmail->password)) throw new InvalidArgumentException("Missing smtp password");
+		if(empty($selfmail->from['adresse'])) throw new InvalidArgumentException("Missing smtp adresse from");
+		if(empty($selfmail->from['name'])) throw new InvalidArgumentException("Missing smtp name from");
 	}
 
 
@@ -67,34 +70,118 @@ class Mail
 		//
 		if(!self::configured($selfmail))
 		{
-			selfmail->$host=Config::get("mail.host");
-			selfmail->$port=Config::get("mail.port");
-			selfmail->$secure=Config::get("mail.encryption");
-			selfmail->$type="html";
-			selfmail->$username=Config::get("mail.username");
-			selfmail->$password=Config::get("mail.password");
+			$selfmail->host=Config::get("mail.host");
+			$selfmail->port=Config::get("mail.port");
+			$selfmail->secure=Config::get("mail.encryption");
+			$selfmail->type="html";
+			$selfmail->username=Config::get("mail.username");
+			$selfmail->password=Config::get("mail.password");
+			$selfmail->from['adresse']=Config::get("mail.from")['adresse'];
+			$selfmail->from['name']=Config::get("mail.from")['name'];
 		}
 		//
-		$selfmail->$transport = Swift_SmtpTransport::newInstance(selfmail->$host, selfmail->$port, selfmail->$secure)
-		  ->setUsername(selfmail->$username)
-		  ->setPassword(selfmail->$password);
+		self::check($selfmail);
+		//
+		$selfmail->transport = Swift_SmtpTransport::newInstance($selfmail->host, $selfmail->port, $selfmail->secure)
+		  ->setUsername($selfmail->username)
+		  ->setPassword($selfmail->password);
 
-		$mailer = Swift_Mailer::newInstance($selfmail->$transport);
+		// $message = Swift_Message::newInstance('Activation de compte Touhfat Al Arouss')
+		//   ->setFrom(array('job@touhfatalarouss.com' => 'Équipe Touhfat Al Arouss'))
+		//   ->setTo(array('youssefhad2@gmail.com'))
+		//   ->setBody($body , "text/html");
+		 //var_dump($selfmail->transport);
 
-		$subject=is_null($selfmail->$subject)?Config::get('mail.subject'):$selfmail->$subject;
+		$mailer = Swift_Mailer::newInstance($selfmail->transport);
+		$subject=is_null($selfmail->subject)?Config::get('mail.subject'):$selfmail->subject;
 
 		//
 		//The Message
 		$message = Swift_Message::newInstance($subject);
 		$message->setBody($body , $type);
+		$message->setFrom(array($selfmail->from['adresse'] => $selfmail->from['name']));
+
+
+		// Check to
 		//
 		if(!is_null($selfmail->too) && !empty($selfmail->too) )
-			$message->setTo($selfmail->too[]);
+			$message->setTo($selfmail->too);
 		else throw new InvalidArgumentException("Missing mail to", 1);
-		
+
+		//
+		// Attaches
+		if(!is_null($selfmail->attachmnt) && count($selfmail->attachmnt)>0)
+		foreach ($selfmail->attachmnt as $key => $value) 
+		{
+			$name="";
+			$filee="";
+			//
+			foreach ($value as $key2 => $value2) {
+				if($key2==0) $filee=$value2;
+				else if($key2==1) $name=$value2;
+			}
+			if(empty($name)) 
+				{ 
+					$message->attach(Swift_Attachment::fromPath($filee));
+				}
+			else 
+				{  
+					$message->attach(Swift_Attachment::fromPath($filee)->setFilename($name));
+				}
+		}
+
+		//
+		// CC
+		if(!is_null($selfmail->cc) && count($selfmail->cc)>0)
+		{
+			
+			$r=array();
+			//
+			foreach ($selfmail->cc as $key => $value) 
+			{
+				$name="";
+				$mail="";
+				//
+				foreach ($value as $key2 => $value2) {
+					if($key2=="mail") { $mail=$value2; }
+					else if($key2=="name") { $name=$value2; }
+				}
+				//
+				if(empty($name)) $r[]=$mail;
+				else $r[$mail]=$name;
+			}
+			//
+			$message->setCC($r);
+		}
+
+		//
+		// CCI
+		if(!is_null($selfmail->cci) && count($selfmail->cci)>0)
+		{
+			
+			$r=array();
+			//
+			foreach ($selfmail->cci as $key => $value) 
+			{
+				$name="";
+				$mail="";
+				//
+				foreach ($value as $key2 => $value2) {
+					if($key2=="mail") { $mail=$value2; }
+					else if($key2=="name") { $name=$value2; }
+				}
+				//
+				if(empty($name)) $r[]=$mail;
+				else $r[$mail]=$name;
+			}
+			//
+			$message->setBcc($r);
+		}
+
 		//
 		// Send
 		$result = $mailer->send($message);
+		return $result;
 
 
 
@@ -263,7 +350,7 @@ class Mail
 				);
 		}
 		//
-		$this->atachmnt=$r2;
+		$this->attachmnt=$r2;
 		return $this;
 	}
 
@@ -282,16 +369,36 @@ class Mail
 	public function cc()
 	{
 		$r=func_get_args();
-		if(!is_array($r)) $r= array( $r );
-		$this->cc=$r;
+		$mail="";
+		$name="";
+		$r2=array();
+		foreach ($r as $key => $value) 
+			foreach ($value as $key2 => $value2)
+				{
+					if(is_numeric($key2)) { $mail= $value2; $name=""; }
+					else { $mail= $key2; $name=$value2; }
+					$r2[]=array("name"=>$name,"mail"=>$mail);
+				}
+
+		$this->cc=$r2;
 		return $this;
 	}
 
 	public function cci($from)
 	{
 		$r=func_get_args();
-		if(!is_array($r)) $r= array( $r );
-		$this->cci=$r;
+		$mail="";
+		$name="";
+		$r2=array();
+		foreach ($r as $key => $value) 
+			foreach ($value as $key2 => $value2)
+				{
+					if(is_numeric($key2)) { $mail= $value2; $name=""; }
+					else { $mail= $key2; $name=$value2; }
+					$r2[]=array("name"=>$name,"mail"=>$mail);
+				}
+
+		$this->cci=$r2;
 		return $this;
 	}
 
