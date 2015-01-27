dcf4af8d64a063ca5ff73ac9d8dee6c5485ed939
Youssefhad <youssefhad2@gmail.com>
2015-01-27 22:58:41 +0000

    update

--- core/Access/Routes_2.php
+++ core/Access/Routes_2.php
@@ -3,10 +3,11 @@
 /**
 * Routes 2
 */
-class Routes_2
+class Routes
 {
 	private static $requests=array();
 	private static $filters=array();
+	private static $current=null;
 
 	public static function get($uri,$callback)
 	{
@@ -14,11 +15,27 @@ class Routes_2
 
 	}
 
-	public static function addCallableGet($uri,$callback)
+	protected static function convert(&$url)
 	{
+		if($url=="/") 
+		{ 
+			$value="project_home"; 
+			$url="";
+		}
+		else  
+		{
+			$value=$url;
+			$url="/".$url; 
+		}
+		return $value;
+	}
+
+	public static function addCallableGet($url,$callback)
+	{
+		$name=self::convert($url);
 		$r = array(
-			'name' => "$uri" , 
-			'url' => "/".$uri , 
+			'name' => "$name" , 
+			'url' => $url , 
 			'callback' => $callback,
 			'methode' => "get",
 			"filtre" => null
@@ -27,8 +44,8 @@ class Routes_2
 		self::$requests[]=$r;
 
 		$r = array(
-			'name' => "$uri"."/" , 
-			'url' => "/".$uri."/" , 
+			'name' => "$name"."/" , 
+			'url' => $url."/" , 
 			'callback' => $callback,
 			'methode' => "get",
 			"filtre" => null
@@ -45,7 +62,7 @@ class Routes_2
 			'callback' => $callback[1],
 			'methode' => "get",
 			"filtre" => $callback[0]
-			}
+			);
 
 		//
 		self::$requests[]=$r;
@@ -87,71 +104,97 @@ class Routes_2
 				{
 					if($value["methode"]=="post" && Res::isPost())
 					{
-						array_shift($params);
-						//
-						self::callBefore();
-						//
-						//new filter
-						$ok=true;
-						$falseok=null;
-						$oks=array();
-						//
-						$filter=$value["filtre"];
-						if(is_string($filtre))
-						{
-							if(!empty($filtre))
-							{
-								self::callFilter($filtre,$ok,$falseok);
-							}
-						}
-						// self::$_request[$key] => $filtre
-						else if(is_array($filtre))
-						{
-							if(!empty($filtre))
-							{
-								self::callFilters($filtre,$ok,$falseok);
-							}
-						}
-
-						// run the route callback
-						if($ok) { self::runRoute($value,$params); }
+						echo "1";
+						$ok=exec($params,$value);
+						break;
+					}
+					else if($value["methode"]=="post" && !Res::isPost())
+					{
+						echo "2";
+						$ok=0;
+					}
+					else if($value["methode"]=="get")
+					{
+						$ok=self::exec($params,$value);
+						break;
 					}
 
 				}
 			}
+			if($ok==0) 
+				Errors::r_404();
+				//echo "non";
 		}
+		else self::showMaintenance();
 	}
 
-	public static function callBefore()
+	protected static function exec($params,&$one)
+	{
+		array_shift($params);
+		//
+		self::callBefore();
+		//
+		$ok=true;
+		$falseok=null;
+		$oks=array();
+		//
+		$filtre=$one["filtre"];
+		if(is_string($filtre))
+		{
+			if(!empty($filtre))
+			{
+				self::callFilter($filtre,$ok,$falseok);
+			}
+		}
+		// self::$_request[$key] => $filtre
+		else if(is_array($filtre))
+		{
+			if(!empty($filtre))
+			{
+				self::callFilters($filtre,$ok,$falseok);
+			}
+		}
+
+		// run the route callback
+		if($ok) { self::runRoute($one,$params); }
+		//if the filter is false
+		else { $ok=self::falseFilter($falseok); }
+		//
+		self::callAfter();
+		$ok=1;
+		return $ok;
+	}
+
+	protected static function callBefore()
 	{
 		call_user_func(App::$Callbacks['before']);
 	}
 
-	public static function callAfter()
+	protected static function callAfter()
 	{
 		call_user_func(App::$Callbacks['after']);
 	}
 
-	public static function CheckUrl()
+	protected static function CheckUrl()
 	{
 		return isset($_GET['url'])?'/'.$_GET['url']:'/';
 	}
 
-	public static function CheckMaintenance($url)
+	protected static function CheckMaintenance($url)
 	{
 		if(!Config::get("maintenance.activate") || in_array($url, Config::get("maintenance.outRoutes")))
 			return true;
 		else return false;
 	}
 
-	public static function Replace()
+	protected static function Replace()
 	{
 		for ($i=0; $i < count(self::$requests); $i++) 
-			if (strpos(self::$requests[$i]['uri'],'{}') !== false) 
-					self::$requests[$i]['uri']=str_replace('{}', '(.*)?', self::$requests[$i]['uri']); 
+			if (strpos(self::$requests[$i]['url'],'{}') !== false) 
+					self::$requests[$i]['url']=str_replace('{}', '(.*)?', self::$requests[$i]['url']); 
 	}
 
-	public static function addFilter($name,$callback,$falsecall=null)
+	protected static function addFilter($name,$callback,$falsecall=null)
 	{
 		$r = array(
 			'name' => $name,
@@ -164,22 +207,22 @@ class Routes_2
 
 	public static function filter($name,$callback,$falsecall=null)
 	{
-		addFilter($name,$callback,$falsecall);
+		self::addFilter($name,$callback,$falsecall);
 	}
 
-	public static function getFilterCallback($name)
+	protected static function getFilterCallback($name)
 	{
 		return self::$filters[$name];
 	}
 
-	public static function callFilter($filtre,&$ok,&$falseok)
+	protected static function callFilter($filtre,&$ok,&$falseok)
 	{
 		$call=self::$filters[$filtre];
 		$ok=call_user_func($call);
 		if(!$ok) { $falseok=$filtre;  }
 	}
 
-	public static function callFilters($filtre,&$ok,&$falseok)
+	protected static function callFilters($filtre,&$ok,&$falseok)
 	{
 		foreach ($filtre as $key => $value) {
 			$call=self::$filters[$value];
@@ -188,10 +231,25 @@ class Routes_2
 		}
 	}
 
-	public static function runRoute($request,$params)
+	protected static function runRoute($request,$params)
 	{
 		self::$current=$request["name"];
 		return call_user_func_array($request["callback"], $params);
+	}
+
+	protected static function falseFilter($key)
+	{
+		$call=self::$filters[$key]['falsecall'];
+		if(isset($call) && !empty($call))
+		{
+			return call_user_func($call);
+		}
+	}
+
+	protected static function showMaintenance()
+	{
+		if(Config::get("maintenance.maintenanceEvent")=="string") echo Config::get("maintenance.maintenanceResponse");
+		else if(Config::get("maintenance.maintenanceEvent")=="link") Url::redirect(Config::get("maintenance.maintenanceResponse"));
 	}
 	
 }
