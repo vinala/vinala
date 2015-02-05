b8569eff47d0d5bd59ccf9b6a2f1d51ef927dfed
Youssefhad <youssefhad2@gmail.com>
2015-02-05 22:58:04 +0000

    edit the class Storage

--- core/Storage/Storage.php
+++ core/Storage/Storage.php
@@ -5,7 +5,7 @@
 */
 class Storage
 {
-	public $disk="uu";
+	public $disk;
 	protected $storagePath;
 	protected $basePath;
 
@@ -36,6 +36,16 @@ class Storage
 	protected function checkDiskExiste($value)
 	{
 		return is_dir(Sys::$app."/storage/file/".$value);
+	}
+
+	protected function isDir($value)
+	{
+		return is_dir($this->storagePath."/".$value);
+	}
+
+	protected function path($value)
+	{
+		return Sys::$app."/storage/file/".$this->disk."/".$value;
 	}
 
 	public static function disk($name)
@@ -107,7 +117,7 @@ class Storage
 			$oldContent=$self->get($name);
 			$self->put($name,($content.$oldContent));	
 		}
-		else throw new InvalidArgumentException("There is no file calls $name to prepend");
+		else throw new InvalidArgumentException("There is no file calls $name");
 	}
 
 	protected function appendNonStatic($name,$content)
@@ -117,7 +127,7 @@ class Storage
 			$oldContent=$this->get($name);	
 			$this->put($name,($oldContent.$content));	
 		}
-		else throw new InvalidArgumentException("There is no file calls $name to append");
+		else throw new InvalidArgumentException("There is no file calls $name");
 	}
 
 	protected static function appendStatic($name,$content)
@@ -128,29 +138,202 @@ class Storage
 			$oldContent=$self->get($name);
 			$self->put($name,($oldContent.$content));	
 		}
-		else throw new InvalidArgumentException("There is no file calls $name to append");
+		else throw new InvalidArgumentException("There is no file calls $name");
 	}
 
-	protected function sizeNonStatic($name,$content)
+	protected function sizeNonStatic($name)
 	{
 		if($this->exists($name))		
 		{
-			$oldContent=$this->get($name)
-			$this->put($name,($oldContent.$content));	
+			return filesize($this->path($name));
 		}
-		else throw new InvalidArgumentException("There is no file calls $name to append");
+		else throw new InvalidArgumentException("There is no file calls $name");
 	}
 
-	protected static function sizeStatic($name,$content)
+	protected static function sizeStatic($name)
 	{
 		$self=new self();
 		if($self->exists($name))
 		{
-			$oldContent=$self->get($name);
-			$self->put($name,($oldContent.$content));	
+			return filesize($self->path($name));	
 		}
-		else throw new InvalidArgumentException("There is no file calls $name to append");
+		else throw new InvalidArgumentException("There is no file calls $name");
 	}
+
+	protected function deleteNonStatic($name)
+	{
+		if($this->exists($name))		
+		{
+			return unlink($this->path($name));
+		}
+		else throw new InvalidArgumentException("There is no file calls $name");
+	}
+
+	protected static function deleteStatic($name)
+	{
+		$self=new self();
+		//
+		if($self->exists($name))
+		{
+			return unlink ($self->path($name));	
+		}
+		else throw new InvalidArgumentException("There is no file calls $name");
+	}
+
+	protected function copyNonStatic($name1,$name2)
+	{
+		if($this->exists($name))		
+		{
+			$content=$this->get($name1);
+			return $this->put($name2,$content);
+		}
+		else throw new InvalidArgumentException("There is no file calls $name");
+	}
+
+	protected static function copyStatic($name1,$name2)
+	{
+		$self=new self();
+		if($self->exists($name))
+		{
+			$content=$self->get($name1);
+			return $self->put($name2,$content);
+		}
+		else throw new InvalidArgumentException("There is no file calls $name");
+	}
+
+	protected function moveNonStatic($name1,$name2)
+	{
+		if($this->exists($name))		
+		{
+			$content=$this->get($name1);
+			$this->put($name2,$content);
+			$this->delete($name1);
+		}
+		else throw new InvalidArgumentException("There is no file calls $name");
+	}
+
+	protected static function moveStatic($name1,$name2)
+	{
+		$self=new self();
+		if($self->exists($name))
+		{
+			$content=$self->get($name1);
+			$self->put($name2,$content);
+			$self->delete($name1);
+		}
+		else throw new InvalidArgumentException("There is no file calls $name");
+	}
+
+	protected function lasteditNonStatic($name)
+	{
+		if($this->exists($name))		
+		{
+			return filemtime($this->path($name));
+		}
+		else throw new InvalidArgumentException("There is no file calls $name");
+	}
+
+	protected static function lasteditStatic($name)
+	{
+		$self=new self();
+		if($self->exists($name))
+		{
+			return filemtime($self->path($name));
+		}
+		else throw new InvalidArgumentException("There is no file calls $name");
+	}
+
+	protected function filesNonStatic($name="")
+	{
+		if($this->isDir($name))		
+		{
+			$files=array();
+			$r=scandir($this->path($name));
+			foreach ($r as $key => $value) {
+				if(is_file($this->storagePath."/".$value)) $files[]=$value;
+			}
+			return $files;
+		}
+		else throw new InvalidArgumentException("There is no directory calls $name");
+	}
+
+	protected static function filesStatic($name="")
+	{
+		$self=new self();
+		if($self->isDir($name))
+		{
+			$files=array();
+			$r=scandir($self->path($name));
+			foreach ($r as $key => $value) {
+				if(is_file($self->storagePath."/".$value)) $files[]=$value;
+			}
+			return $files;
+		}
+		else throw new InvalidArgumentException("There is no directory calls $name");
+	}
+
+	protected function directorysNonStatic($name="")
+	{
+		if($this->isDir($name))		
+		{
+			$files=array();
+			$r=scandir($this->path($name));
+			foreach ($r as $key => $value) {
+				if(is_dir($this->storagePath."/".$value)) $files[]=$value;
+			}
+			return $files;
+		}
+		else throw new InvalidArgumentException("There is no directory calls $name");
+	}
+
+	protected static function directorysStatic($name="")
+	{
+		$self=new self();
+		if($self->isDir($name))
+		{
+			$files=array();
+			$r=scandir($self->path($name));
+			foreach ($r as $key => $value) {
+				if(is_dir($self->storagePath."/".$value)) $files[]=$value;
+			}
+			return $files;
+		}
+		else throw new InvalidArgumentException("There is no directory calls $name");
+	}
+
+	protected function makeDirNonStatic($name)
+	{
+		if($this->isDir($name))		
+		return mkdir($this->storagePath."/".$name);
+	}
+
+	protected static function makeDirStatic($name="")
+	{
+		$self=new self();
+		return mkdir($self->storagePath."/".$name);
+	}
+
+	protected function deleteDirNonStatic($name="")
+	{
+		if($this->isDir($name))		
+		{
+			return rmdir($self->storagePath."/".$name);
+		}
+		else throw new InvalidArgumentException("There is no directory calls $name");
+	}
+
+	protected static function deleteDirStatic($name="")
+	{
+		$self=new self();
+		if($self->isDir($name))
+		{
+			return rmdir($self->storagePath."/".$name);
+		}
+		else throw new InvalidArgumentException("There is no directory calls $name");
+	}
+
+
+
 
 
 
@@ -161,6 +344,17 @@ class Storage
 	    	case 'get': return call_user_func_array(array($this, "getNonStatic"), $args); break;
 	    	case 'prepend': return call_user_func_array(array($this, "prependNonStatic"), $args); break;
 	    	case 'append': return call_user_func_array(array($this, "appendNonStatic"), $args); break;
+	    	case 'size': return call_user_func_array(array($this, "sizeNonStatic"), $args); break;
+	    	case 'delete': return call_user_func_array(array($this, "deleteNonStatic"), $args); break;
+	    	case 'copy': return call_user_func_array(array($this, "copyNonStatic"), $args); break;
+	    	case 'move': return call_user_func_array(array($this, "moveNonStatic"), $args); break;
+	    	case 'lastModified': return call_user_func_array(array($this, "lasteditNonStatic"), $args); break;
+	    	case 'files': return call_user_func_array(array($this, "filesNonStatic"), $args); break;
+	    	case 'directories': return call_user_func_array(array($this, "directorysNonStatic"), $args); break;
+	    	case 'makeDirectory': return call_user_func_array(array($this, "makeDirNonStatic"), $args); break;
+	    	case 'deleteDirectory': return call_user_func_array(array($this, "deleteDirNonStatic"), $args); break;
+	    	default : throw new Whoops\Exception\ErrorException("Call to undefined method Storage::$name()");
+	    	
 	    }
 	}
 
@@ -172,6 +366,17 @@ class Storage
 	    	case 'get': return call_user_func_array(array("Storage", "getStatic"), $args); break;
 	    	case 'prepend': return call_user_func_array(array("Storage", "prependStatic"), $args); break;
 	    	case 'append': return call_user_func_array(array("Storage", "appendStatic"), $args); break;
+	    	case 'size': return call_user_func_array(array("Storage", "sizeStatic"), $args); break;
+	    	case 'delete': return call_user_func_array(array("Storage", "deleteStatic"), $args); break;
+	    	case 'copy': return call_user_func_array(array("Storage", "copyStatic"), $args); break;
+	    	case 'move': return call_user_func_array(array("Storage", "moveStatic"), $args); break;
+	    	case 'lastModified': return call_user_func_array(array("Storage", "lasteditStatic"), $args); break;
+	    	case 'files': return call_user_func_array(array("Storage", "filesStatic"), $args); break;
+	    	case 'directories': return call_user_func_array(array("Storage", "directorysStatic"), $args); break;
+	    	case 'makeDirectory': return call_user_func_array(array("Storage", "makeDirStatic"), $args); break;
+	    	case 'deleteDirectory': return call_user_func_array(array("Storage", "deleteDirStatic"), $args); break;
+	    	default : throw new Whoops\Exception\ErrorException("Call to undefined method Storage::$name()");
+	    	
 	    }
 	}
 }
