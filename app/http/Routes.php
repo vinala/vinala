c7a7676e4ac6c59442da8b21d9c29fc6bc41b99d
Youssefhad <youssefhad2@gmail.com>
2015-02-07 16:09:42 +0000

    change Filters.php and Routes.php location

---
+++ app/http/Routes.php
@@ -0,0 +1,17 @@
+<?php 
+
+/*
+|----------------------------------------------
+| App Routes
+|----------------------------------------------
+| hna route dial l application dialk aya route 
+| khask t7eto hena 
+|
+|
+*/
+
+
+Routes::get("/",function()
+{	
+	return View::make('hello');
+});
