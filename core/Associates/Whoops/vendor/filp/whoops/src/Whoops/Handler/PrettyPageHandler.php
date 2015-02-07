6bcdafa1733444c86d10f7f5d836cb982698ff82
Youssefhad <youssefhad2@gmail.com>
2015-02-07 13:41:53 +0000

    update PrettyPageHandler for Debug Class

--- core/Associates/Whoops/vendor/filp/whoops/src/Whoops/Handler/PrettyPageHandler.php
+++ core/Associates/Whoops/vendor/filp/whoops/src/Whoops/Handler/PrettyPageHandler.php
@@ -126,13 +126,13 @@ class PrettyPageHandler extends Handler
 
         $inspector = $this->getInspector();
         $frames    = $inspector->getFrames();
-
+        //
         $code = $inspector->getException()->getCode();
 
         if ($inspector->getException() instanceof \ErrorException) {
             $code = Misc::translateErrorCode($code);
         }
-
+        //error_log("###".Formatter::formatExceptionPlain($inspector)."###");
         // List of variables that will be passed to the layout template.
         $vars = array(
             "page_title" => $this->getPageTitle(),
@@ -166,6 +166,7 @@ class PrettyPageHandler extends Handler
                 "Cookies"               => $_COOKIE,
                 "Session"               => isset($_SESSION) ? $_SESSION :  array(),
                 "Environment Variables" => $_ENV,
+                "alt" => array('a' => "A" , "b" => "B" ),
             ),
         );
 
@@ -182,7 +183,7 @@ class PrettyPageHandler extends Handler
 
         $helper->setVariables($vars);
         $helper->render($templateFile);
-
+        //
         return Handler::QUIT;
     }
 
@@ -463,5 +464,114 @@ class PrettyPageHandler extends Handler
     public function setResourcesPath($resourcesPath)
     {
         $this->addResourcePath($resourcesPath);
+    }
+
+
+
+    /**
+     * @return int|null
+     */
+    public function handleUserDebug($name2,$array)
+    {
+        if (!$this->handleUnconditionally()) {
+            // Check conditions for outputting HTML:
+            // @todo: Make this more robust
+            if (php_sapi_name() === 'cli') {
+                // Help users who have been relying on an internal test value
+                // fix their code to the proper method
+                if (isset($_ENV['whoops-test'])) {
+                    throw new \Exception(
+                        'Use handleUnconditionally instead of whoops-test'
+                        .' environment variable'
+                    );
+                }
+
+                return Handler::DONE;
+            }
+        }
+
+        // @todo: Make this more dynamic
+        $helper = new TemplateHelper();
+
+        $templateFile = $this->getResource("views/layout.html.php");
+        $cssFile      = $this->getResource("css/whoops.base.css");
+        $zeptoFile    = $this->getResource("js/zepto.min.js");
+        $jsFile       = $this->getResource("js/whoops.base.js");
+
+        if ($this->customCss) {
+            $customCssFile = $this->getResource($this->customCss);
+        }
+
+        //$inspector = $this->getInspector();
+        $frames    = array();
+
+        $code = 1;
+
+        //
+
+        // List of variables that will be passed to the layout template.
+        $vars = array(
+            "page_title" => $this->getPageTitle(),
+
+            // @todo: Asset compiler
+            "stylesheet" => file_get_contents($cssFile),
+            "zepto"      => file_get_contents($zeptoFile),
+            "javascript" => file_get_contents($jsFile),
+
+            // Template paths:
+            "header"      => $this->getResource("views/header.html.php"),
+            "frame_list"  => $this->getResource("views/frame_list.html.php"),
+            "frame_code"  => $this->getResource("views/frame_code.html.php"),
+            "env_details" => $this->getResource("views/env_details.html.php"),
+
+            "title"          => $this->getPageTitle(),
+            "name"           => array("Fiesta","Debug"),
+            "message"        => $name2,
+            "code"           => $code,
+            "plain_exception" => "DebugStatus thrown with message 'hhhh'\n
+\n
+Stacktrace:\n
+#8 DebugStatus in C:\wamp\www\fiesta\core\Debug.php:10\n
+#7 Debug:stop in C:\wamp\www\fiesta\app\Routes.php:17\n
+#6 App:{closure} in <#unknown>:0\n
+#5 call_user_func_array in C:\wamp\www\fiesta\core\Access\Routes_2.php:405\n
+#4 Routes:runRoute in C:\wamp\www\fiesta\core\Access\Routes_2.php:303\n
+#3 Routes:exec in C:\wamp\www\fiesta\core\Access\Routes_2.php:246\n
+#2 Routes:run in C:\wamp\www\fiesta\core\Ini.php:147\n
+#1 App:run in C:\wamp\www\fiesta\bootstrap\start.php:28\n
+#0 require in C:\wamp\www\fiesta\public\index.php:13",
+            "frames"         => $frames,
+            "has_frames"     => !!count($frames),
+            "handler"        => $this,
+            "handlers"       => array($this),
+
+            "tables"      => array(
+                "Selected parameters" => $array,
+                "Server/Request Data"   => $_SERVER,
+                "GET Data"              => $_GET,
+                "POST Data"             => $_POST,
+                "Files"                 => $_FILES,
+                "Cookies"               => $_COOKIE,
+                "Session"               => isset($_SESSION) ? $_SESSION :  array(),
+                "Environment Variables" => $_ENV,
+                
+            ),
+        );
+
+        if (isset($customCssFile)) {
+            $vars["stylesheet"] .= file_get_contents($customCssFile);
+        }
+
+        // Add extra entries list of data tables:
+        // @todo: Consolidate addDataTable and addDataTableCallback
+        $extraTables = array_map(function ($table) {
+            return $table instanceof \Closure ? $table() : $table;
+        }, $this->getDataTables());
+        $vars["tables"] = array_merge($extraTables, $vars["tables"]);
+
+        $helper->setVariables($vars);
+        $helper->render($templateFile);
+
+        return Handler::QUIT;
     }
 }
