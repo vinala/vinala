2edc7694006a3216d0a55e044e78f0bf1c1c60ca
Youssefhad <youssefhad2@gmail.com>
2015-02-16 00:26:30 +0000

    edit cache config array

--- app/config/cache.php
+++ app/config/cache.php
@@ -4,10 +4,36 @@
 return array(
 
 	/*
+	|---------------------------------------------
+	| Default Cache Store
+	|---------------------------------------------
+	|
+	| Hna l mode dial storage dial l cache
+	| disponibles : file - database
+	|
+	*/
+
+	'default' => "file",
+
+
+	/*
 	|----------------------------------------------
-	| Project name
+	| Cache options
 	|----------------------------------------------
 	*/
+
+	"options" => [ 
+
+		"file" => [ 
+			"driver" => "file",
+			'location'=>"storage/cache"
+		],
+
+		"database" => [ 
+			"driver" => "database",
+			'database'=>null
+		],
+	],
 
 	'location'=>"storage/cache",
 
