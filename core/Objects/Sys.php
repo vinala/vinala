39f606fe2125b1683dd53d90d1aed80ddb0c78a4
youssefhad <youssefhad2@gmail.com>
2015-02-17 13:05:11 +0000

    replace class path to Path.php

--- core/Objects/Sys.php
+++ core/Objects/Sys.php
@@ -26,36 +26,3 @@ class Sys
 	}
 }
 
-/**
-* System Class
-*/
-class Path
-{
-	public static $root;
-	public static $base;
-	public static $path;
-	//
-	public static $app;
-	public static $core;
-	public static $public;
-	//
-	public static $libs;
-
-	public static function ini()
-	{
-		$path="$_SERVER[REQUEST_URI]";
-		$paths=explode('/', $path);
-		$root="";
-		$base="";
-		
-		self::$root=$root;
-		self::$base=$base;
-		//
-		self::$app="app";
-		self::$core="core";
-		self::$public="public";
-		//
-		self::$path="http://$_SERVER[HTTP_HOST]/";
-	}
-}
-Path::ini();
