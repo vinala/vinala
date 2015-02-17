2684c4058e040e2612bfb6439d30488bf912afcb
youssefhad <youssefhad2@gmail.com>
2015-02-17 13:04:50 +0000

    add class path

---
+++ core/Access/Path.php
@@ -0,0 +1,60 @@
+<?php 
+
+/**
+* System Class
+*/
+class Path
+{
+	public static $root;
+	public static $host;
+	public static $base;
+	public static $path="1";
+	//
+	public static $app;
+	public static $core;
+	public static $public;
+	//
+	public static $libs;
+
+	public static function ini()
+	{
+		$path="$_SERVER[REQUEST_URI]";
+		$paths=explode('/', $path);
+		$root="";
+		$base="";
+		//
+		self::$root=self::getRoot();
+		self::$host=self::getHost();
+		self::$base=$base;
+		//
+		self::$app=self::$host.self::$root."app";
+		//self::$app="app";
+		self::$core=self::$host.self::$root."core";
+		self::$public=self::$host.self::$root."public";
+		//
+		self::$path="http://$_SERVER[HTTP_HOST]/";
+	}
+
+	public static function getRoot()
+	{
+		$root="$_SERVER[REQUEST_URI]";
+		$parts=explode("/", $root);
+		//
+		$folder="";
+		//
+		for ($i=1; $i < count($parts) ; $i++) 
+		{
+			$folder.=$parts[$i]."/";
+			if( ! (new Fiesta\Filesystem\Filesystem)->isDirectory($folder)) break;
+		}
+		//
+		return $folder;
+	}
+
+	public static function getHost()
+	{
+		return "http://$_SERVER[HTTP_HOST]/";
+	}
+
+}
+
