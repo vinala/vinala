3b503c986ea4b5918aede76210c078428daca27e
youssefhad <youssefhad2@gmail.com>
2015-07-05 19:46:58 +0000

    edit view creation

--- public/home/ajax/new_view.php
+++ public/home/ajax/new_view.php
@@ -1,6 +1,6 @@
 <?php 
 
-$class=$_POST['new_view_class_name'];
+// $class=$_POST['new_view_class_name'];
 $file=$_POST['new_view_file_name'];
 //
 $Root="../../";
@@ -9,7 +9,7 @@ if(!file_exists($Root."../app/views/$file.php"))
 		$myfile = fopen($Root."../app/views/$file.php", "w");
 		$txt = "<?php\n\n";
 
-		$txt.="/**\n* class de view $class\n*/\n\nclass $class\n{\n\t# Code...\n}";
+		$txt.="/**\n* View file  : $file\n*/\n\n";
 
 		fwrite($myfile, $txt);
 		fclose($myfile);
