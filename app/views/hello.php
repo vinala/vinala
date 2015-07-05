df83c1199ab7780b6711119b8a51076c640038bb
youssef <youssefhad2@gmail.com>
2015-07-05 15:16:43 +0000

    editing hello view

--- app/views/hello.php
+++ app/views/hello.php
@@ -1,75 +1,22 @@
 <head>
-	<link href='http://fonts.googleapis.com/css?family=Ubuntu:300,400,500' rel='stylesheet' type='text/css'>
+
+	<link rel="stylesheet" href="app/library/bootstrap-3.3.1.min.css">
+	<link rel="stylesheet" href="app/library/bootstrap-theme-3.3.1.min.css">
+	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+
 	<?php 
 		Html::charset("utf-8"); 
 		Html::title();
 		Html::favicon(Path::$public."/favicon.ico");
+		Libs::css("hello");
+		Libs::js("hello");
 	?>
 
-	<script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-	<script type="text/javascript">
-	$( document ).ready(function() {
-	  // setTimeout(fade (), 6000);
-	  var myVar = setInterval(function(){ fade() }, 1000);
-
-	  function fade () {
-		$( "#welcom" ).fadeTo( "slow", 1 );
-		clearInterval(myVar);
-		}
-	});
-	</script>
+	
 
 	<style type="text/css">
-		body
-		{
-			padding:0px;
-			margin:0px;
-			background:#5b1d79
-		}
-
-		.img
-		{
-			width:260px;
-			margin:0pt auto;
-			display:block;
-			margin-top: 30px;
-		}
-
-		#welcom
-		{
-			opacity:0;
-			color:white;
-			text-align: center;
-			font-family: 'Lato', sans-serif;
-			width:185px;
-			margin: 0px auto;
-		}
-
-		.f_line
-		{
-			background: white;
-			width: 20px;
-			height: 2px;
-			margin-top: 8px;
-			float: left;
-		}
-		.l_line
-		{
-			background: white;
-			width: 185px;
-			width: 20px;
-			height: 2px;
-			margin-top: 8px;
-			display: inline-block;
-			float: right;
-		}
-		.text
-		{
-			background: #5b1d79;
-			display: inline-block;
-			z-index: 3;
-			width: 145px;
-		}
+		
+		
 	</style>
 
 </head>
@@ -88,6 +35,9 @@
 					echo " ".Config::get('app.owner');
 			?>
 		</div>
+	</div>
+	<div class="bottom_panel" id="bottom_panel">
+		<a href="<?php echo Config::get("panel.route") ?>"><div class="btn hello_button" id="login">Fiesta Panel</div></a>
 	</div>
 </body>
 
