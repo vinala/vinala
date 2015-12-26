c96d906e1222f2ab014bea363bce4ade39f54a21
Youssef <youssefhad2@gmail.com>
2015-12-26 16:02:21 +0000

    add route to manage panel ajax

---
+++ app/controllers/panel.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Fiesta\Core\Panel;
+//
+use Fiesta\Core\MVC\Controller\Controller;
+use Fiesta\Core\Glob\App;
+use Fiesta\Core\Database\Seeder;
+use Fiesta\Core\Database\Migration;
+use Fiesta\Core\Config\Config;
+use Fiesta\Core\Database\Schema;
+use Fiesta\Core\Database\Database;
+
+/**
+* class de controller PanelController
+* This is system file, DON'T REMOVE IT !!
+*/
+
+class Panel
+{
+
+}
+
