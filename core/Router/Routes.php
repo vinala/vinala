bc9434f42b00f50f385c15f34ee4cb340e81396c
Youssef <youssefhad2@gmail.com>
2015-12-26 15:46:18 +0000

    panel ajax route for seeder creation

--- core/Router/Routes.php
+++ core/Router/Routes.php
@@ -10,6 +10,7 @@ use Fiesta\Core\Router\Exception\RouteNotFoundException;
 use Fiesta\Core\Http\Errors;
 use Fiesta\Core\Glob\App;
 use Fiesta\Core\Access\Url;
+use Fiesta\Core\Panel\Seeds;
 use Fiesta\Core\Panel\Migrations;
 
 
@@ -592,7 +593,8 @@ if(Config::get('panel.enable'))
 	});
 
 	Routes::get(Config::get('panel.route')."/{op}",function($op){
-		switch ($op) {
+		switch ($op) {			
+			case 'new_seed': Seeds::add(); break;			
 			case 'new_migration': Migrations::add(); break;
 		}
 	});
