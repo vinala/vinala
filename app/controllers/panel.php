e2d98196700685794edb4097059b3d722456bf84
Youssef <youssefhad2@gmail.com>
2015-12-26 17:19:45 +0000

    manage model creation

--- app/controllers/panel.php
+++ app/controllers/panel.php
@@ -335,3 +335,38 @@ class Link
 		return $txt;
 	}
 }
+
+/**
+* Model class
+*/
+class Model
+{
+	public static function create()
+	{
+		
+		$class=$_POST['new_models_class_name'];
+		$file=$_POST['new_models_file_name'];
+		$table=$_POST['new_models_table_name'];
+
+		
+		if(!file_exists("../app/models/$file.php"))
+			{
+				$myfile = fopen("../app/models/$file.php", "w");
+				$txt = self::set($class , $table);
+
+				fwrite($myfile, $txt);
+				fclose($myfile);
+				//
+				echo "Le model a été creé";
+			}
+			else echo "Le fichier deja existe";
+	}
+
+	public static function set($class , $table)
+	{
+		$txt = "<?php\n\nuse Fiesta\Core\MVC\Model\Model;\n\n";
+		$txt.="class $class extends Model\n{\n\t//Name of the table in database\n\tpublic static ".'$table'."='$table';\n\tprotected static ".'$foreignKeys=array();'."\n\n}";
+		//
+		return $txt;
+	}
+}
