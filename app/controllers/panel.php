316358ff586e3c6306bf957e539fb2d01505410c
Youssef <youssefhad2@gmail.com>
2015-12-26 18:12:31 +0000

    manage costume execution and rollback of migration

--- app/controllers/panel.php
+++ app/controllers/panel.php
@@ -12,14 +12,8 @@ use Fiesta\Core\Database\Database;
 use Fiesta\Core\Objects\Date_Time as Time;
 
 /**
-* class de controller PanelController
-* This is system file, DON'T REMOVE IT !!
+* File for Panel features
 */
-
-class Panel
-{
-
-}
 
 /**
 * Seeds class
@@ -236,6 +230,115 @@ class Migrations
 				
 			}
 			else echo "Schema n'est pas annulé".Database::execErr();
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	public static function rollback_cos() /* Beta */
+	{
+		$Root = "../";
+		$r=glob("../app/schemas/*.php");
+
+		$r2=array();
+		$r2=array();
+		foreach ($r as $value) {
+
+		$temp1=explode("schemas/",$value);
+		$temp2=explode("_",$temp1[1]);
+		$temp3=explode(".",$temp2[1]);
+		$ex=$temp3[0];
+		//
+
+		if($ex==$_POST['exec_cos_migrate_select'])
+			{
+				$r2[]=$ex;
+				$r3[]=$temp2[0];
+
+			}
+		}
+		$v="";
+		//
+		if(count($r2)>1)
+		{
+			for ($i=1; $i < count($r2); $i++) { 
+				error_log($r3[$i].'*/*'.$r3[$i-1]);
+				if($r3[$i]>=$r3[$i-1])
+				{
+					$v="../app/schemas/".$r3[$i]."_".$r2[$i].".php";
+					$full_name=$r3[$i]."_".$r2[$i];
+				}
+			}
+		}
+		else
+		{
+			$v="../app/schemas/".$r3[0]."_".$r2[0].".php";
+			$full_name=$r3[0]."_".$r2[0];
+		}
+
+
+		try {
+			include_once $v;
+			if(down())
+			{
+				Migration::updateRegister($full_name,"rollback",$Root);
+				echo "Schéma annulée";
+			}
+			else echo Database::execErr();
+		} catch (Exception $e) {
+			echo $e->getMessage();
+		}
+	}
+
+	public static function exec_cos() /* Beta */
+	{
+		$Root = "../";
+		$r=glob("../app/schemas/*.php");
+
+		$r2=array();
+		$r2=array();
+		foreach ($r as $value) {
+
+		$temp1=explode("schemas/",$value);
+		$temp2=explode("_",$temp1[1]);
+		$temp3=explode(".",$temp2[1]);
+		$ex=$temp3[0];
+		//
+		if($ex==$_POST['exec_cos_migrate_select'])
+			{
+				$r2[]=$ex;
+				$r3[]=$temp2[0];
+			}
+		}
+		$v="";
+		$full_name="";
+		//
+		if(count($r2)>1)
+		{
+			for ($i=1; $i < count($r2); $i++) { 
+				error_log($r3[$i].'*/*'.$r3[$i-1]);
+				if($r3[$i]>=$r3[$i-1])
+				{
+					$v="../app/schemas/".$r3[$i]."_".$r2[$i].".php";
+					$full_name=$r3[$i]."_".$r2[$i];
+				}
+			}
+		}
+		else
+		{
+			$v="../app/schemas/".$r3[0]."_".$r2[0].".php";
+			$full_name=$r3[0]."_".$r2[0];
+		}
+
+
+		try {
+			include_once $v;
+			if(up())
+			{
+				Migration::updateRegister($full_name,"exec",$Root);
+				echo "Schéma executé";
+			}
+			else echo Database::execErr();
 		} catch (Exception $e) {
 			echo $e->getMessage();
 		}
