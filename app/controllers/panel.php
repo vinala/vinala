eafa49ba51f6b47cc4221957ec536755d1990400
Youssef <youssefhad2@gmail.com>
2015-12-26 16:04:30 +0000

    add function for seeder creation

--- app/controllers/panel.php
+++ app/controllers/panel.php
@@ -20,6 +20,44 @@ class Panel
 
 }
 
+/**
+* Seeds class
+*/
+class Seeds
+{
+
+	public static function add()
+	{
+		$nom=$_POST['seedname_name'];
+		$Root="../";
+		//
+		if(!file_exists($Root."app/seeds/$nom.php"))
+		{
+		 	$myfile = fopen($Root."app/seeds/$nom.php", "w");
+			$txt = self::set($nom);
+			//
+			fwrite($myfile, $txt);
+			fclose($myfile);
+			//
+			echo "le seeder est créé";
+		}
+		else echo "Le fichier deja existe";
+	}
+
+	public static function set($nom)
+	{
+		$txt = "<?php\n\nuse Fiesta\Core\Database\Seeder;\n\n";
+		$txt.="/**\n* class de seeder $nom\n*/\n\nclass $nom extends Seeder\n{\n";
+
+		//datatable name
+		$txt.="\t/*\n\t* Name of DataTable\n\t*/\n\tpublic ".'$table="tbl_user";'."\n\n";
+
+			//run
+		$txt.="\t/*\n\t* Run the Database Seeder\n\t*/\n\tpublic function run()\n\t{\n\t\t".'$dataTable = array();'."\n\t\t//\n\t\t".'$dataTable[] = array(/* Data Fields */);'."\n\t\t//\n\t\t".'Schema::table($this->table)->insert($dataTable);'."\n\t}\n}";
+
+		return $txt;
+	}
+}
 
 
 /**
