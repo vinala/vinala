5d9966b29457b7f424f700f7ae1edb5d0f8c1e96
Youssef <youssefhad2@gmail.com>
2016-01-04 22:37:20 +0000

    add more message type

--- core/Logging/Log.php
+++ core/Logging/Log.php
@@ -9,32 +9,24 @@ use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
 use Fiesta\Core\Objects\Date_Time;
 use Fiesta\Core\Objects\Date_Time as Time;
+use Fiesta\Core\Objects\String;
+use Fiesta\Core\Objects\Table;
 
 /**
 * 
 */
 class Log
 {
-	
+
 	public static function ini()
 	{
 		ini_set("log_errors", 1);
 		ini_set("error_log", Config::get("loggin.errors"));
 	}
 
-	protected static function setLogTarget($target)
+	public static function log($message , $time = true)
 	{
-		// if( $target == "error") 
-		// else if( $target == "exceptions") 
-		// else return $target;
-	}
-
-	public static function log($message , $target = "error")
-	{
-		$myfile = fopen( Config::get('loggin.errors') , "a+");
-		$txt = self::getTime( Date_Time::now() )." $message\n";
-		fwrite($myfile, $txt);
-		fclose($myfile);
+		error_log($message);
 	}
 
 	protected static function getTime($timestamp)
@@ -44,34 +36,95 @@ class Log
 
 	protected static function endLog($path)
 	{
-		$myfile = fopen($path, "a+");
 		$txt = "";
-		for ($i=0; $i < 20; $i++) $txt.="-";
-		$txt.="\n";
-		fwrite($myfile, $txt);
-		fclose($myfile);
+		for ($i=0; $i < 50; $i++) $txt.="-";
+		self::log($txt."\n" , false);
+	}
+
+	protected static function tabulation()
+	{
+		$lenght= 21+2+(String::lenght(Date_Time::getTimezone()));
+		$txt = "";
+		for ($i=0; $i < $lenght; $i++) $txt .= " ";
+		return $txt." ";
+	}
+
+	protected static function TraceTabulation()
+	{
+		$tab = self::getTime( Date_Time::now() )." Trace : ";
+		$txt = "";
+		for ($i=0; $i < String::lenght($tab); $i++) $txt .= " ";
+		return $txt." ";
 	}
 
 	protected static function Trace($exception)
 	{
 		$i = 1; $txt="";
 		$traces = $exception->getTrace();
-		// die(var_dump($traces));
 		//
-		foreach ($traces as $trace) if(array_key_exists("file", $trace)) $txt .= "\t\t\t#$i / $trace[file]:$trace[line]\n";
+		foreach ($traces as $trace) if(array_key_exists("file", $trace)) { $txt .= "\n".self::TraceTabulation()."#$i / $trace[file]:$trace[line]"; $i++; }
 
 		return $txt;
 	}
 
-	public static function setException($Exception)
+	public static function exception($Exception)
 	{
-		$path = Config::get("loggin.exceptions");
-		$log = new Logger('');
-		$log->pushHandler(new StreamHandler($path));
-		$log->addWarning("Message : ".$Exception->getMessage());
-		$log->addWarning("Location : ".$Exception->getFile()." in line ".$Exception->getLine());
-		$log->addWarning("Class : ".get_class($Exception));
-		$log->addWarning("Trace : ".self::Trace($Exception));
-		self::endLog($path);
+		$txt = "" ;
+		$txt .= "Exception : ".$Exception->getMessage() . "\n";
+		$txt .= self::tabulation()."Location : ".$Exception->getFile()." in line ".$Exception->getLine() . "\n";
+		$txt .= self::tabulation()."Class : ".get_class($Exception) . "\n";
+		$txt .= self::tabulation()."Trace : ".self::Trace($Exception) ;
+
+		self::log($txt);
+	}
+
+	public static function error($message , $data = array())
+	{
+		self::log("Error : " . $message);
+		if( ! empty($data) ) 
+			self::log(self::tabulation(). Table::toString ($data),false);
+		self::log("" , false);
+	}
+
+	public static function warning($message , $data = array())
+	{
+		self::log("Warning : " . $message);
+		if( ! empty($data) ) self::log(self::tabulation(). Table::toString ($data),false);
+		self::log("" , false);
+	}
+
+	public static function info($message , $data = array())
+	{
+		self::log("Info : " . $message);
+		if( ! empty($data) ) self::log(self::tabulation(). Table::toString ($data),false);
+		self::log("" , false);
+	}
+
+	public static function debug($message , $data = array())
+	{
+		self::log("Debug : " . $message);
+		if( ! empty($data) ) self::log(self::tabulation(). Table::toString ($data),false);
+		self::log("" , false);
+	}
+
+	public static function notice($message , $data = array())
+	{
+		self::log("Notice : " . $message);
+		if( ! empty($data) ) self::log(self::tabulation(). Table::toString ($data),false);
+		self::log("" , false);
+	}
+
+	public static function critical($message , $data = array())
+	{
+		self::log("Critical : " . $message);
+		if( ! empty($data) ) self::log(self::tabulation(). Table::toString ($data),false);
+		self::log("" , false);
+	}
+
+	public static function alert($message , $data = array())
+	{
+		self::log("Alert : " . $message);
+		if( ! empty($data) ) self::log(self::tabulation(). Table::toString ($data),false);
+		self::log("" , false);
 	}
 }
