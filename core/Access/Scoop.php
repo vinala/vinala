9bafcbe18decd210d82a66ac582cd8442ad2abb8
Youssef <youssefhad2@gmail.com>
2016-01-09 22:46:13 +0000

    add get function for Route::get

--- core/Access/Scoop.php
+++ core/Access/Scoop.php
@@ -1,8 +1,20 @@
 <?php 
 
 use Fiesta\Core\MVC\View\View;
+use Fiesta\Core\Router\Route;
 
+/**
+ * Views
+ */
 function view($value,$data=null)
 {
 	View::make($value,$data);
+}
+
+/**
+ * Route
+ */
+function get($uri,$callback,$subdomains=null)
+{
+	Route::get($uri,$callback,$subdomains);
 }
