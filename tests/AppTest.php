825b0ba0b7069b38375b24b84084fd198d005ea2
Youssef <youssefhad2@gmail.com>
2016-01-10 22:14:16 +0000

    new class for test

---
+++ tests/AppTest.php
@@ -0,0 +1,16 @@
+<?php 
+
+/**
+* 
+*/
+class AppTest extends PHPUnit_Framework_TestCase
+{
+	public function testIfRun()
+	{
+		require_once __DIR__.'/../core/Ini.php';
+		
+		$app = Fiesta\Core\Glob\App::run();
+		
+		$this->assertTrue( $app );
+	}
+}
