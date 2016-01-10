6446e2fbd69e800194b039e6896b8fb67093f014
Youssef <youssefhad2@gmail.com>
2016-01-10 13:25:57 +0000

    remove panel route treatment

--- core/Router/Routes.php
+++ core/Router/Routes.php
@@ -591,27 +591,5 @@ class Routes
 	
 }
 
-if(Config::get('panel.enable'))
-{
-	Routes::get(Config::get('panel.route'),function(){
-		include '../vendor/fiesta/'.Config::get('panel.folder').'/index.php';
-	});
 
-	Routes::get(Config::get('panel.route')."/{op}",function($op){
-		switch ($op) {
-			case Config::get('panel.ajax')['new_seed']: Seeds::add(); break;
-			case Config::get('panel.ajax')['exec_migration']: Migrations::exec(); break;
-			case Config::get('panel.ajax')['rollback_migration']: Migrations::rollback(); break;
-			case Config::get('panel.ajax')['new_migration']: Migrations::add(); break;
-			case Config::get('panel.ajax')['new_controller']: Controller::create(); break;
-			case Config::get('panel.ajax')['new_dir_lang']: Lang::createDir(); break;
-			case Config::get('panel.ajax')['new_file_lang']: Lang::createFile(); break;
-			case Config::get('panel.ajax')['new_link']: Link::create(); break;
-			case Config::get('panel.ajax')['new_model']: Model::create(); break;
-			case Config::get('panel.ajax')['new_view']: View::create(); break;
-			case Config::get('panel.ajax')['exec_cos_migration']: Migrations::exec_cos(); break;
-			case Config::get('panel.ajax')['rollback_cos_migration']: Migrations::rollback_cos(); break;
-		}
-	});
-}
 
