9081101fb4a4e062462d3e234ec44df847cd6889
Youssef <youssefhad2@gmail.com>
2016-01-11 01:34:11 +0000

    new class for tasting

---
+++ core/Testing/TestCase.php
@@ -0,0 +1,42 @@
+<?php 
+
+namespace Fiesta\Core\Testing;
+
+use PHPUnit_Framework_TestCase;
+use Fiesta\Core\Glob\App;
+
+
+/**
+* TestCase Class For testing
+*/
+class TestCase extends PHPUnit_Framework_TestCase
+{
+	
+	public function run($path)
+	{
+		self::call(__DIR__."/../");
+		//
+		return self::check();
+	}
+
+	public static function mock()
+	{
+		return $app = self::instance(__DIR__."/");
+	}
+
+	public static function call($path)
+	{
+		require_once $path.'core/Ini.php';
+	}
+
+	public static function instance($path)
+	{
+		return App::run("test",$path,false,false);
+	}
+
+	public static function check()
+	{
+		return App::run("test",$path,false,false);
+	}
+}
+
