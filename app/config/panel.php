9e7fbf6564702fb5861f13ba1c2f7a26403163df
Youssef <youssefhad2@gmail.com>
2016-01-11 14:19:53 +0000

    update config panel file structure

--- app/config/panel.php
+++ app/config/panel.php
@@ -1,41 +1,38 @@
 <?php 
 
-
 return array(
+	
 
 	/*
 	|----------------------------------------------
 	| Panel Activation
 	|----------------------------------------------
-	| hna true ila bghiti l panel tb9a kheda
-	| meni tsali menha redha false...pour 
-	| votre sécurité
-	| 
+	|  hna true ila bghiti l panel tb9a kheda
+	|  meni tsali menha redha false...pour 
+	|  votre sécurité
 	*/
 
 	'enable'=> true,
+
 
 	/*
 	|----------------------------------------------
 	| Panel Route
 	|----------------------------------------------
-	| hna route dial l panel
-	| route khas ikon diffrent 3la had les valeurs:
-	| 
-	| 
+	|  hna route dial l panel
 	*/
 
 	'route'=>'fiesta',
+
 
 	/*
 	|----------------------------------------------
 	| Panel Path
 	|----------------------------------------------
-	| here the path of the panel index, you can 
-	| search in the internet to change the panel, 
-	| for your security you should change the panel
-	| folder name
-	| 
+	|  here the path of the panel index, you can 
+	|  search in the internet to change the panel, 
+	|  for your security you should change the panel
+	|  folder name
 	*/
 
 	'path'=>'vendor/fiesta/panel/index.php',
@@ -43,44 +40,72 @@ return array(
 
 	/*
 	|----------------------------------------------
-	| Panel passwords
+	| Panel Passwords
 	|----------------------------------------------
-	| hna katktb les mot de passe dial panel bach 
-	| bihom t9der tdkhol l panel dialk par default
-	| fihom 1234 o 5678 nta t9der tbdlhom
+	|  hna katktb les mot de passe dial panel bach 
+	|  bihom t9der tdkhol l panel dialk par default
+	|  fihom 1234 o 5678 nta t9der tbdlhom
 	*/
 
 	'password1'=>'1234',
 	'password2'=>'5678',
 
+
 	/*
 	|----------------------------------------------
 	| First Configuration
 	|----------------------------------------------
-	| 
+	|  The framework will set true if you passed 
+	|  the first configuration
 	*/
 
 	'configured' => false,
+
 
 	/*
 	|----------------------------------------------
 	| Ajax Routes
 	|----------------------------------------------
-	| this is links of ajax functions
+	|  This is links of ajax functions
 	*/
+
 	'ajax' => array(
-		'new_seed' => 'new_seed', 				// for new seeds
-		'exec_migration' => 'exec_migration', 			// to exec migrations
-		'rollback_migration' => 'rollback_migration', 		// to rollback migrations
-		'new_migration' => 'new_migration', 			// for new migrations
-		'new_controller' => 'new_controller', 			// for new controllers
-		'new_dir_lang' => 'new_dir_lang', 			// for new language folder
-		'new_file_lang' => 'new_file_lang', 			// for new language file
-		'new_link' => 'new_link', 				// for new links file
-		'new_model' => 'new_model', 				// for new models
-		'new_view' => 'new_view', 				// for new views
-		'exec_cos_migration' => 'exec_cos_migration', 		// to exec costume migrations
-		'rollback_cos_migration' => 'rollback_cos_migration', 	// to rollback costume migrations
-	),	
+
+		// for new seeds
+			'new_seed' => 'new_seed',
+
+		// to exec migrations
+			'exec_migration' => 'exec_migration',
+
+		// to rollback migrations
+			'rollback_migration' => 'rollback_migration', 
+
+		// for new migrations
+			'new_migration' => 'new_migration',
+
+		// for new controllers
+			'new_controller' => 'new_controller',
+
+		// for new language folder
+			'new_dir_lang' => 'new_dir_lang',
+
+		// for new language file
+			'new_file_lang' => 'new_file_lang',
+
+		// for new links file
+			'new_link' => 'new_link',
+
+		// for new models
+			'new_model' => 'new_model',
+
+		// for new views
+			'new_view' => 'new_view',
+
+		// to exec costume migrations
+			'exec_cos_migration' => 'exec_cos_migration',
+
+		// to rollback costume migrations
+			'rollback_cos_migration' => 'rollback_cos_migration',
+	),
 
 );
