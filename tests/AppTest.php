3fe39d83b2c9db7d0427315da58e0b134f80785d
Youssef <youssefhad2@gmail.com>
2016-01-11 18:03:49 +0000

    redefine AppTest

--- tests/AppTest.php
+++ tests/AppTest.php
@@ -6,18 +6,18 @@ use Fiesta\Core\Testing\TestCase;
 /**
 * AppTestClass for testing
 */
-class AppTest
+class AppTest extends \PHPUnit_Framework_TestCase
 {
 
 	/**
 	 * Check if PHPUnit accept the Framework test 
 	 */
-	public function testIfRun()
+	public function test()
 	{
-		require_once __DIR__.'/../core/Testing/TestCase.php'; 
+		require_once __DIR__.'/../core/Testing/TestCase.php';
 		//
 		$app = TestCase::run();
 		
-		$this->assertTrue( $app );
+		return $this->assertTrue( $app );
 	}
 }
