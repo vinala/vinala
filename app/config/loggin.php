368ef0cd4b8e58058f665815c4ea39522b08a054
Youssef <youssefhad2@gmail.com>
2016-01-14 21:23:09 +0000

    documentation for loggin.php

--- app/config/loggin.php
+++ app/config/loggin.php
@@ -6,33 +6,30 @@ return array(
 	|----------------------------------------------
 	| Allow Debug
 	|----------------------------------------------
-	| hena bach tkheli l framework i debugger les 
-	| erreur o les exception dialo ila kan false 
-	| maghadich i affachier les erreur
-	| 
-	| 
+	|  Here to make the framework shows errors and
+	|  exceptions, false to show friendly messages
+	|  and true to debug
 	*/
 
-	'debug' => false,
+	'debug' => !false,
 
 	/*
 	|----------------------------------------------
 	| Error Debug Message
 	|----------------------------------------------
-	| ila kan l parametre dial debug fih false
-	| l framework ghadi y affichier had l msag
-	| 
+	|  If loggin.debug was false the framework will
+	|  show this message
 	*/
+
 	'msg' => "Ohlala! il semble que quelque chose s'ait mal passé",
 
 	/*
 	|----------------------------------------------
 	| Error log
 	|----------------------------------------------
-	| hana ghadi t3tih l fichier li ghadi ydir fih 
-	| les error_log
-	| par defaut kayn fichier f storage
-	| 
+	|  The path of log file where Fiesta store errors
+	|  by default the framework use this path 
+	|  'app/storage/logs/fiesta.log'
 	*/
 
 	'log' => 'app/storage/logs/fiesta.log',
@@ -41,11 +38,7 @@ return array(
 	|----------------------------------------------
 	| Error simple page background color
 	|----------------------------------------------
-	| hana ghadi t3tih l couleur dial l background
-	| dial la page simple dial l erreur ya3ni lpage 
-	| li katkhroj dial l erreur ila kan debug fiha 
-	| false
-	| 
+	|  The color background of simple page error
 	*/
 
 	'bg' => '#a4003a',
