ae0d14ea736d360ba5a8ee73e7ef27fe20437323
Youssef <youssefhad2@gmail.com>
2016-01-14 20:48:48 +0000

    edit documentation for license.php

--- app/config/license.php
+++ app/config/license.php
@@ -16,20 +16,10 @@ return array(
 	|----------------------------------------------
 	| Blocked Messages
 	|----------------------------------------------
-	| had l blasa pour les evenemnt li kaytdar meni
-	| tatkoon mblocker site dialk ola ghir chi page
-	| we7da
 	*/
 
-	// hena khas tkoon (string ola link) string
-	// pour message o link pour rederiction
-	// 'webEvent'=>"string", 
-
-	// hena khas t7t l message ila event string
-	// ola lien ila kan l event link
 	'webResponse'=>"this website is <b>not authorised</b> in the internet, please if you think that's an error try again",
 
-	// message pour l block dial page
 	'pageMsg'=>"this page is <b>not authorised</b> in the internet, please if you think that's an error try again",
 
 );
