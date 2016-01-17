29c7fede3e50cc4e10927ef90551c68de450f498
Youssef <youssefhad2@gmail.com>
2016-01-17 14:47:58 +0000

    add function to clear screen

--- core/Http/Http.php
+++ core/Http/Http.php
@@ -7,8 +7,19 @@ namespace Fiesta\Core\Http;
 */
 class Http
 {
+	/**
+	 * To exit compilation and show string
+	 */
 	public static function abort($string = "")
 	{
 		die($string);
+	}
+
+	/**
+	 * To clear screen from echoed data
+	 */
+	public static function clear()
+	{
+		ob_end_clean();
 	}
 }
