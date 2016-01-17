8aac697d0999ec5d01a41f1723763a4092fb109a
Youssef <youssefhad2@gmail.com>
2016-01-17 23:04:59 +0000

    create view for database error

---
+++ app/views/errors/database.tpl.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="utf-8"/>
+    <title>Database error</title>
+    {Libs::css("error")}
+
+</head>
+<body>
+    <div>It seems that we have a problem to connect to database server</div>
+</body>
