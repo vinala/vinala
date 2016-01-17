dd4c6734773e6b7e465bb99f99970bb280b39a50
Youssef <youssefhad2@gmail.com>
2016-01-17 23:04:22 +0000

    create view for 404 error

---
+++ app/views/errors/404.tpl.php
@@ -0,0 +1,9 @@
+<head>
+    <meta charset="utf-8"/>
+    <title>404</title>
+    {Libs::css("error")}
+
+</head>
+<body>
+    <div>It seems that the page you are looking for is not there</div>
+</body>
