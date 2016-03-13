080027da3b9ab1a6f263234c63d93d5395f66332
Youssef <youssefhad2@gmail.com>
2016-03-13 14:47:37 +0000

    First view in plugin

---
+++ plugins/QR/views/code.tpl.php
@@ -0,0 +1,10 @@
+<!DOCTYPE>
+<html>
+<head>
+	<title></title>
+</head>
+<body style="margin:0px;padding:0px">
+<img src='https://chart.googleapis.com/chart?chs={$size}x{$size}&cht=qr&chl={$text}&chld=H|0'/>
+
+</body>
+</html>
