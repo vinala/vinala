938f8665d0777bc3699b1ae3da427e6e4c58ab10
Youssef <youssefhad2@gmail.com>
2016-04-09 23:50:55 +0100

    add bootstrap file for console

---
+++ bootstrap/console.php
@@ -0,0 +1,14 @@
+<?php 
+
+
+require __DIR__.'/../vendor/autoload.php';
+
+
+/*
+|----------------------------------------------
+| Run the console
+|----------------------------------------------
+| launch the Fiesta console
+*/
+
+Fiesta\Kernel\Foundation\Application::console();
