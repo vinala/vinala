6b812fd42d1d7726ee96ac261d0f38efd063d001
Youssef <youssefhad2@gmail.com>
2016-04-17 12:23:44 +0100

    new config file for console to customize console

---
+++ config/console.php
@@ -0,0 +1,24 @@
+<?php 
+
+return array(
+
+	"new_schema" => "schema:new",
+	"exec_schema" => "schema:exec",
+	"rollback_schema" => "schema:rollback",
+	//
+	"dir_lang" => "lang:dir",
+	"file_lang" => "lang:file",
+	//
+	"new_link" => "link:new",
+	//
+	"new_model" => "model:new",
+	//
+	"new_view" => "view:new",
+	//
+	"new_controller" => "controller:new",
+	//
+	"new_seed" => "seed:new",
+	"exec_seed" => "seed:exec",
+	//
+	"get_routes" => "routes:get"
+);
