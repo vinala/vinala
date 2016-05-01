3c11b348de1ef7b0a3efc49e4341af7f9873b4ff
Youssef <youssefhad2@gmail.com>
2016-05-01 20:19:07 +0100

    update documentation and retrieve salute command

--- app/console/commands/salute.php
+++ app/console/commands/salute.php
@@ -11,8 +11,7 @@ class Salute extends Commands
      *
      * @var string
      */
-    // protected $key = 'say:hello {firstName : The first name} {lastName? : The last name} {--option}';
-    protected $key = 'say:hellos {firstName : The first name}';
+    protected $key = 'say:hello {firstName : The first name} {lastName? : The last name} {--option}';
 
     /**
      * The console command description.
@@ -28,28 +27,9 @@ class Salute extends Commands
      */
     public function handle()
     {
-        // $firstName = $this->argument('firstName');
+        $firstName = $this->argument('firstName');
+        $lastName = $this->argument('lastName');
         //
-        // $this->info("Hello ".$firstName." ");
-        // $this->comment("Hello ".$firstName." ");
-        // $this->question("Hello ".$firstName." ");
-        // $this->error("Hello ".$firstName." ");
-        // $age = $this->password("how old are you ?");
-        // echo $age;
-        // $this->progress();
-        // $this->info("this info");
-        // $this->comment("this comment");
-        // $this->question("this question");
-        // $this->error("this error");
-        $name = $this-> ask('whats your name ? ');
-        $nickname = $this-> ask('whats your nickname ? ');
-        $age = $this-> ask('whats your age ? ');
-        $sex = $this-> confirm('are you a women ? ');
-
-        $title = ! $sex ? "Mr" : "Ms" ;
-        //
-        $this->info("hello $title $name $nickname you have $age years old");
-
-
+        $this->write("Hello ".$firstName." ".$lastName);
     }
 }
--- changes.md
+++ changes.md
@@ -5,7 +5,6 @@ New features:
 
  * Supporting Plugins 
  * Supporting Console Command-line [Documentation](https://github.com/fiesta-framework/Docs/blob/3.2/src/console.md#console)
- 
  * Change the name from Fiesta to Pikia
  * New Logo (inspired by Davide Giliati - https://dribbble.com/DavideGiliati)
  * New welcome page
