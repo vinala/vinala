477f974a20a45c0c6c213e9e2793dcf357f90d0b
Youssef <youssefhad2@gmail.com>
2016-05-28 19:37:38 +0100

    rename references

--- database/seeds/SeedsCaller.php
+++ database/seeds/SeedsCaller.php
@@ -11,7 +11,7 @@ class SeedsCaller extends Seeder
 	/**
 	 * All Seeders to call
 	 */
-	public static function refernces()
+	public static function references()
 	{
 		return [
 			// userTableSeeder::class,
