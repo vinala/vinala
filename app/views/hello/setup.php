286ce8e5f824f445a9fa1edd2f9c1d6efe425044
Youssef <youssefhad2@gmail.com>
2016-05-31 22:30:32 +0100

    make fst msg in english

--- app/views/hello/setup.php
+++ app/views/hello/setup.php
@@ -7,44 +7,27 @@
 
 	<div style="height:60px"></div>
 
-	<div id="lang-step">
-		<div class="fst-config-icon"></div>
-		<div class="fst-config-cont">
-			<form class="fst-config-form" id="fst-config-form">
-				<div class="fst-config-lang-combo">
-					<select class="form-control" id="sel1" name="langue">
-					    <option value="fr">Français</option>
-						<!-- <option value="ar">العربية</option> -->
-						<option value="en" selected>English</option>
-			        </select>
-				</div>
-
-				<div style="margin-top:20px">
-					<input type="submit" class="btn hello_button_hover" value="Next" name="nxt" id="nxt"   />
-				</div>
-			</form>
-		</div>
-	</div>
-	<div id="fr-db-msg-step"   style="display:none">
+	
+	<div id="fr-db-msg-step">
 		<!-- <h1 class="conf_title">General</h1> -->
 		<!-- <div class="progres"><div class="progres_1"></div></div> -->
-		<img src="https://gitlab.com/uploads/project/avatar/1202141/hey_race_icon2.png" class="fst-config-icon">
+		<div class="fst-config-icon"></div>
 		<div class="fst-config-text">
 			<div class="fst-config-pargraph">
-				Bienvenue dans Lighty. Avant de nous lancer, nous avons besoin de certaines informations sur votre base de données. Il va vous falloir réunir les informations suivantes pour continuer.
+				Welcome to Lighty. Before we launch, we need some information about your database. You'll have to fill the following information to proceed.
 			</div>
 			<div class="fst-config-pargraph">
-				<li>Nom de la base de données</li>
-				<li>Nom d’utilisateur MySQL</li>
-				<li>Mot de passe de l’utilisateur</li>
-				<li>Adresse de la base de données</li>
-				<li>Préfixe de table</li>
+				<li>Name of the database</li>
+				<li>User name</li>
+				<li>User password</li>
+				<li>Server of the database</li>
+				<li>Table Prefix</li>
 			</div>
 			<div class="fst-config-pargraph">
-				Vous devriez normalement avoir reçu ces informations de la part de votre hébergeur. Si vous ne les avez pas, il vous faudra contacter votre hébergeur afin de continuer. Si vous êtes prêt(e)…
+				You would normally have received these information from your host. If you do not, you should contact your hosting provider to continue. If you are ready...
 			</div>
 			<div style="margin-top:20px">
-				<input type="submit" class="btn hello_button" value="C'est parti !" name="nxt" id="nxt"   />
+				<input type="submit" class="btn hello_button_hover" value="Let's go !" name="nxt" id="nxt"   />
 			</div>
 		</div>
 	</div>
