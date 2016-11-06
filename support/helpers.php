4bd27873be21e54c389c45b834967af4fb79dc9d
Youssef <youssefhad2@gmail.com>
2016-11-06 20:06:38 +0000

    add file for user helpers

---
+++ support/helpers.php
@@ -0,0 +1,7 @@
+<?php 
+
+/*
+* Vinala uses a variety of global functions calls helpers, 
+* however, you are free to create your own helpers here.
+* just create a simple function not public or static
+*/
