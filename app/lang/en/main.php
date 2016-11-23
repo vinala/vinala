adc4cdb8b780dea5764168b791fc917cea1823c0
Youssef <youssefhad2@gmail.com>
2016-11-23 21:31:45 +0000

    add message for 404 error

--- app/lang/en/main.php
+++ app/lang/en/main.php
@@ -5,5 +5,6 @@ return [
 	'error' => [
 			'regular_message' => 'Ohlala! It seems that something went wrong',
 			'database_message' => 'It seems that we have a problem to connect to database server',
+			'404_message' => 'It seems that the page you are looking for is not there',
 		],
 	];
