143f7d379187587d7da0f687312db09e88d3e064
Youssef <youssefhad2@gmail.com>
2016-11-23 21:21:09 +0000

    update english lang file

--- app/lang/en/main.php
+++ app/lang/en/main.php
@@ -1,5 +1,9 @@
 <?php 
 
-return array(
+return [
 	'welcome' =>  "Welcome",
-	);
+	'error' => [
+			'regular_message' => 'Ohlala! It seems that something went wrong',
+			'database_message' => 'It seems that we have a problem to connect to database server',
+		],
+	];
