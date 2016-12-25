789604f57875d120d3fafa787326b77b5c1461ab
Youssef <youssefhad2@gmail.com>
2016-12-25 18:28:23 +0000

    update app config file

--- config/app.php
+++ config/app.php
@@ -1,81 +1,72 @@
 <?php
 
-use Vinala\Kernel\Foundation\Application;
  
-	return array(
+return [
 
 	/*
 	|----------------------------------------------------------
 	| Project name
 	|----------------------------------------------------------
 	| Your project name
-	*/
- 
-	'project'=>'', 
- 
+	|
+	**/
+	'project' => '' , 
+
+
 	/*
-	|---------------------------------------------------------- 
-	| Owner name 
-	|---------------------------------------------------------- 
+	|----------------------------------------------------------
+	| Owner name
+	|----------------------------------------------------------
 	| Your name
-	*/ 
+	|
+	**/
+	'owner' => '' , 
  
-	'owner'=>'', 
- 
- 
-	/* 
-	|---------------------------------------------------------- 
+
+	/*
+	|----------------------------------------------------------
 	| Project url 
-	|---------------------------------------------------------- 
+	|----------------------------------------------------------
 	| Your website root link, you should put your 
 	| root link , by default we using App::root 
 	| function to get the root link even if you 
 	| working on localhost
-	*/ 
- 
-	'url'=>Application::root(), 
- 
-	/* 
-	|---------------------------------------------------------- 
-	| HTML Default title 
-	|---------------------------------------------------------- 
-	| Default HTML title
-	*/ 
- 
-	'title'=> 'Lighty PHP Framework',
+	|
+	**/
+	'url' => root() , 
 
-	/* 	
-	|---------------------------------------------------------- 
+
+	/*
+	|----------------------------------------------------------
+	| HTML Default title
+	|----------------------------------------------------------
+	| Default HTML title
+	|
+	**/
+	'title' => 'Vinala PHP Framework' ,
+
+
+	/*
+	|----------------------------------------------------------
 	| Timezone
-	|---------------------------------------------------------- 
+	|----------------------------------------------------------
 	| Here you should set your timezone after that 
 	| whenever you wanna get time, Pikia will give
 	| you exact time for the timezone.
 	| To get all of timezones supported in php 
 	| visite here : http://php.net/manual/en/timezones.php
-	*/ 
+	|
+	**/
+	'timezone' => 'UTC' ,
 
-	'timezone'=> 'UTC',  
- 
-	/* 
-	|---------------------------------------------------------- 
-	| Routing inexists event 
-	|---------------------------------------------------------- 
-	| When HttpNotFoundException trown if unrouted 
-	| parameter was true it will be show to 
-	| exception else the framework will redirect
-	| user to Error::r_404 route,
-	*/ 
- 
-	'unrouted'=> true, 
- 
-	/* 
-	|---------------------------------------------------------- 
+
+	/*
+	|----------------------------------------------------------
 	| Default Character Set 
-	|---------------------------------------------------------- 
+	|----------------------------------------------------------
 	| Default encodage when you using HTML::charset
-	*/ 
+	|
+	**/
+	'charset'=> 'utf-8' , 
  
-	'charset'=> 'utf-8', 
- 
-);
+];
