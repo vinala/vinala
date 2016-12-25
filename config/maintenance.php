a5aacc02b71296d30b40439ade881da915692934
Youssef <youssefhad2@gmail.com>
2016-12-25 17:42:31 +0000

    add paramter for the view that will be displayed if maintenance is activated

--- config/maintenance.php
+++ config/maintenance.php
@@ -17,12 +17,23 @@ return [
 	|----------------------------------------------------------
 	| Routes out of maintenance 
 	|----------------------------------------------------------
-	| List of routes that will not stopped by maintenance filter
+	| List of routes that will not stopped by maintenance 
 	| filter
 	| 
 	**/
 	'out' => [
 		//
 	],
+
+
+	/*
+	|----------------------------------------------------------
+	| Maintenance view
+	|----------------------------------------------------------
+	| The view that will be displayed if maintenance
+	| is activated
+	| 
+	**/
+	'view' => 'errors.maintenance',
 
 ];
