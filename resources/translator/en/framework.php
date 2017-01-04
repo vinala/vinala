495c97775675197f563b75a5f5be9b6b5359ec3f
Youssef <youssefhad2@gmail.com>
2017-01-04 12:25:36 +0000

    small update

--- resources/translator/en/framework.php
+++ resources/translator/en/framework.php
@@ -3,7 +3,7 @@
 return [
 	'welcome' =>  "Welcome",
 	'error' => [
-			'regular_message' => 'Ohlala! It seems that something went wrong',
+			'regular_message' => 'Oops! It seems that something went wrong',
 			'database_message' => 'It seems that we have a problem to connect to database server',
 			'404_message' => 'It seems that the page you are looking for is not there',
 			'maintenance_message' => 'The website is currently undergoing maintenance'
