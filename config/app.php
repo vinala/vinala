2ca0e0077e52a39c5001ceeb29297a9cd16a979f
Youssef <youssefhad2@gmail.com>
2017-01-31 17:17:18 +0000

    make setup variable in app instead of panel configs

--- config/app.php
+++ config/app.php
@@ -1,57 +1,60 @@
 <?php
 
- 
 return [
 
 	/*
 	|----------------------------------------------------------
 	| Project name
 	|----------------------------------------------------------
-	| Your project name
+	|  Your project name
 	|
 	**/
-	'project' => '' , 
+	'project' => '' ,
+
 
 
 	/*
 	|----------------------------------------------------------
 	| Owner name
 	|----------------------------------------------------------
-	| Your name
+	|  Your name
 	|
 	**/
-	'owner' => '' , 
- 
+	'owner' => '' ,
+
+
 
 	/*
 	|----------------------------------------------------------
-	| Project url 
+	| Project url
 	|----------------------------------------------------------
-	| Your website root link, you should put your 
-	| root link , by default we using App::root 
+	|  Your website root link, you should put your 
+	| root link , by default we using Application::root 
 	| function to get the root link even if you 
 	| working on localhost
 	|
 	**/
-	'url' => root() , 
+	'url' => root() ,
+
 
 
 	/*
 	|----------------------------------------------------------
 	| HTML Default title
 	|----------------------------------------------------------
-	| Default HTML title
+	|  Default HTML title
 	|
 	**/
-	'title' => 'Vinala PHP Framework' ,
+	'title' => 'Vinala PHP Framework' , 
+
 
 
 	/*
 	|----------------------------------------------------------
 	| Timezone
 	|----------------------------------------------------------
-	| Here you should set your timezone after that 
-	| whenever you wanna get time, Pikia will give
+	|  Here you should set your timezone after that 
+	| whenever you wanna get time, Vinala will give
 	| you exact time for the timezone.
 	| To get all of timezones supported in php 
 	| visite here : http://php.net/manual/en/timezones.php
@@ -60,13 +63,26 @@ return [
 	'timezone' => 'UTC' ,
 
 
+
 	/*
 	|----------------------------------------------------------
-	| Default Character Set 
+	| Default Character Set
 	|----------------------------------------------------------
-	| Default encodage when you using HTML::charset
+	|  Default encodage when you using HTML::charset
 	|
 	**/
-	'charset'=> 'utf-8' , 
- 
+	'charset' => 'utf-8' , 
+
+
+	/*
+	|----------------------------------------------------------
+	| Setup
+	|----------------------------------------------------------
+	|  The framework will set true if you passed 
+	|  the setup
+	*/
+
+	'setup' => true,
+
+
 ];
