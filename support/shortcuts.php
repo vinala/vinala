b3c1f18a23c89c9c8b618406fe0d7666103863e5
Youssef <youssefhad2@gmail.com>
2017-02-01 19:22:40 +0000

    remove shortcuts file

--- support/shortcuts.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-/*
-* Vinala uses a variety of global functions calls shortcuts, 
-* however, you are free to create your own helpers here.
-* just create a simple function not public or static
-*/
