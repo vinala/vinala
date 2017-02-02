6e76a7a4762701f6c529a0c386fee3972f42afa0
Youssef <youssefhad2@gmail.com>
2017-02-02 23:36:22 +0000

    change view location

---
+++ resources/views/errors/maintenance.php
@@ -0,0 +1,47 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+	    <meta charset="utf-8"/>
+	    <title>The website is currently undergoing maintenance</title>
+	    <style type="text/css">
+            @font-face {
+              font-family: 'segoe';
+              src: url('app/resources/fonts/segoeuil.ttf') format('truetype');
+            }
+
+            body
+            {
+                background: #26324f;
+                margin: 0px;
+                padding: 0px;
+            }
+
+            div 
+            {
+                box-shadow: 0px 3px 6px 3px rgba(0,0,0,0.2);
+                border:1px solid gray;
+                border-radius:5px;
+                display: inline-block;
+                padding:30px;
+                font-size: 16px;
+                /*font: 20px Georgia, "segoe", Times, serif;*/
+                font: 20px "segoe";
+                width: 460px;
+                margin: 60px auto;
+                display: block;
+                background: white;
+                text-align: center;
+            }
+        </style>
+
+	</head>
+	<body>
+
+	    <div>
+	    	The website is currently undergoing maintenance
+	    </div>
+	    
+	</body>
+</html>
