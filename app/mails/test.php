06fbd4523ecd29309304989d260c79e78b15516f
Youssef <youssefhad2@gmail.com>
2017-05-25 18:28:36 +0000

    update

---
+++ app/mails/test.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Mails;
+
+use Vinala\Kernel\Mailing\Mailable;
+
+/**
+ * test Mailable
+ *
+ * @author 
+ * creation time : 2017-05-24 19:00:46 (1495652446)
+ */
+class test extends Mailable
+{
+
+	/**
+	 * Create new mail instance
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		// 
+	}
+
+	/**
+	 * Build the mail
+	 *
+	 * @return $this
+	 */
+	public function build()
+	{
+		// do something
+	}
+
+}
